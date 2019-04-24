b67c8e02a488d6e621882dc97d3a122f93098b7d
scaldy010 <47572824+scaldy010@users.noreply.github.com>
2019-04-24 18:36:35 +0100

    Added parts class

---
+++ src/banner/Createdroid.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banner;
+
+/**
+ *
+ * @author Roryh
+ */
+public class Createdroid
+        
+{
+
+    
+ 
+    
+    Droid r1 = new Droid();
+    
+}
